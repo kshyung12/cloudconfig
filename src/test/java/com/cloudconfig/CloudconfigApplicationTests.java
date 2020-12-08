47a47f6d6b65dd7361e275d88cd8e503b891efef
kshyung12 <kshyung12@crewmate.co.kr>
2020-12-08 11:39:05 +0900

    init

---
+++ src/test/java/com/cloudconfig/CloudconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudconfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
